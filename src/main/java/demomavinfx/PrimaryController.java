2ecc7322b6627dedfb10c1a009639ac0f44e1be3
Sævar Axel Bjarnason <saevar@Svars-MacBook-Air.local>
2025-09-28 19:15:34 +0000

    Smá uppfæring og svoleiðis

--- src/main/java/demomavinfx/PrimaryController.java
+++ src/main/java/demomavinfx/PrimaryController.java
@@ -1,22 +1,31 @@
 package demomavinfx;
+import javafx.scene.image.Image;
 
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 public class PrimaryController {
     @FXML
     private Label NumberLabel;
+    @FXML
+    private ImageView Heisenberg;
     
 
     @FXML
     private void handleButtonClick(ActionEvent event)  {
         int number = (int)(Math.random() * 100);
         NumberLabel.setText(String.valueOf(number));
+
         if(number == 67){
-            
-        }
+            Image mynd = new Image(getClass().getResourceAsStream("/demomavinfx/images/Heisenberg.png"));
+            Heisenberg.setImage(mynd);
+          Heisenberg.setVisible(true);
+       }
+        else
+        Heisenberg.setVisible(false);
     }
 
     
